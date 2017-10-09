c234945a7cc136d40c9452454e9fb0bf9ae098b7
Cyprien Aubry <cyprienaubry@linkpulsion.com>
2017-10-09 15:01:40 +0200

    Creating setting entity

---
+++ src/TwitchPageBundle/Entity/Setting.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace TwitchPageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Setting
+ *
+ * @ORM\Table(name="setting")
+ * @ORM\Entity(repositoryClass="TwitchPageBundle\Repository\SettingRepository")
+ */
+class Setting
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Setting
+     */
+    public function setName($name): Setting
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return Setting
+     */
+    public function setValue($value): Setting
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Setting
+     */
+    public function setType($type): Setting
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+}
+
